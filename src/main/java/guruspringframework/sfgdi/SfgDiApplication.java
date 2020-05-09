ffe3aa4696f5d11f9d4e289b06ee69690204b32c
miki <mpacala00@gmail.com>
2020-05-09 13:32:17 +0200

    solved assignment

--- src/main/java/guruspringframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guruspringframework/sfgdi/SfgDiApplication.java
@@ -1,7 +1,6 @@
 package guruspringframework.sfgdi;
 
 import guruspringframework.sfgdi.controllers.*;
-import guruspringframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
--- src/main/java/guruspringframework/sfgdi/controllers/PetController.java
+++ src/main/java/guruspringframework/sfgdi/controllers/PetController.java
@@ -1,13 +1,21 @@
 package guruspringframework.sfgdi.controllers;
 
 import guruspringframework.sfgdi.services.PetService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Controller
 public class PetController {
 
     private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
 
     public String whichPetIsTheBest(){
         return petService.getPetType();
