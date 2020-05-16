bda597d8124e3f81aefb55b140cedb84711eaea2
mpacala00 <mpacala00@gmail.com>
2020-05-16 14:24:24 +0200

    Adding basic @ComponentScan

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -4,7 +4,14 @@ import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+//this annotataion scans the components in the sfgdi directory (package where this class is placed)
+//ComponentScan works on the whole package tree - the package we specified and all of the packages included
+//@ComponentScan(basePackages = {"guru.springframework.packageName"}) - an example
+//When doing that it is important to tell Spring about all directories, even those which were default previously
+//Classes can also be used instead of packages
+@ComponentScan(basePackages = {"guru.springframework.sfgdi"})
 @SpringBootApplication
 public class SfgDiApplication {
 
