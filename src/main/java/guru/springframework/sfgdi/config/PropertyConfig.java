256b124a8a4d470d22ab1d8d6f2595aa54d2437a
mpacala00 <mpacala00@gmail.com>
2020-05-24 15:38:23 +0200

    Using environmental variables.

--- src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
@@ -1,18 +1,26 @@
 package guru.springframework.sfgdi.config;
 
 import guru.springframework.sfgdi.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 //show spring where are the properties located
 public class PropertyConfig {
 
+    @Autowired
+    Environment env;
+    //env.getProperty("PROPERTY_NAME");
+    //another way to use environment variables
+
     //spring expression language, that's why ${ }
+    //this value is being overwritten because of environmental variable
     @Value("${guru.username}")
     String user;
 
