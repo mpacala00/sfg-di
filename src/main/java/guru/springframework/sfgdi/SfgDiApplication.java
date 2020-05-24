cb188d4a744966833b2115ff8fedf8c0c443a486
mpacala00 <mpacala00@gmail.com>
2020-05-24 15:11:43 +0200

    Reading properties from a file.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,16 +1,12 @@
 package guru.springframework.sfgdi;
 
 import guru.springframework.sfgdi.controllers.*;
+import guru.springframework.sfgdi.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-//this annotataion scans the components in the sfgdi directory (package where this class is placed)
-//ComponentScan works on the whole package tree - the package we specified and all of the packages included
-//@ComponentScan(basePackages = {"guru.springframework.packageName"}) - an example
-//When doing that it is important to tell Spring about all directories, even those which were default previously
-//Classes can also be used instead of packages
 @ComponentScan(basePackages = {"guru.springframework.sfgdi"})
 @SpringBootApplication
 public class SfgDiApplication {
@@ -18,14 +14,12 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
-
 		MyController myController = ctx.getBean("myController", MyController.class);
 
-		//depending on profile there will be a different greeting displayed by myController
-		System.out.println(myController.hello());
+		FakeDataSource fakeDataSource = ctx.getBean("fakeDataSource", FakeDataSource.class);
 
-		System.out.println(ctx.getBean(PropertyInjectedController.class).getGreeting());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).getGreeting());
+		System.out.println(fakeDataSource.getUser());
+
 	}
 
 }
---
+++ src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
@@ -0,0 +1,42 @@
+package guru.springframework.sfgdi.config;
+
+import guru.springframework.sfgdi.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+//show spring where are the properties located
+public class PropertyConfig {
+
+    //spring expression language, that's why ${ }
+    @Value("${guru.username}")
+    String user;
+
+    @Value("${guru.password}")
+    String password;
+
+    @Value("${guru.dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    //this bean will provide the value for the strings above
+    //it will be reading the datasource.properties file
+    private static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer
+                = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
